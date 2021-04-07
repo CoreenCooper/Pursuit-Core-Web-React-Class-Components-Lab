cedf0f5367a42ac47e548f8d8e63a6a2057d7562
Coreen Cooper <coreencooper@pursuit.org>
2021-04-07 14:56:51 -0400

    update ContactList.js to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./App.css";
 import ContactList from "./ContactList";
 import Feed from "./Feed";
+import "./App.css";
 
 // class component /////
 class App extends React.Component {

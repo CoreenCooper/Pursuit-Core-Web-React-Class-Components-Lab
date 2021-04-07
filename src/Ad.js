4448f75677129776c3546f48623aa7c84cdbb991
Coreen Cooper <coreencooper@pursuit.org>
2021-04-07 14:46:57 -0400

    update Feed.js to class component

--- src/Ad.js
+++ src/Ad.js
@@ -16,6 +16,7 @@ export const adInfo = [
   },
 ];
 
+// class component ////////////////
 const index = Math.floor(Math.random() * adInfo.length);
 const ad = adInfo[index];
 
@@ -32,5 +33,19 @@ class Ad extends React.Component {
     );
   }
 }
+
+// functional component
+
+// const Ad = () => {
+//   const index = Math.floor(Math.random() * adInfo.length);
+//   const ad = adInfo[index];
+
+//   return (
+//     <div className="Ad" data-testid="ad">
+//       <img src={ad.src} alt="cool ad" />
+//       <p data-testid="ad-text">{ad.text}</p>
+//     </div>
+//   );
+// };
 
 export default Ad;
